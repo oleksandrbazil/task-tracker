9bd46af850c175dfbbd61f6e2840d066a2cfab49
Oleksandr Bazil <oleksandrbazil@gmail.com>
2019-04-23 18:43:28 +0300

    develop: added devtools extension support

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-export default createStore(reducers, applyMiddleware(sagaMiddleware));
+const enhancers = [];
+const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+if (typeof devToolsExtension === 'function') {
+  enhancers.push(devToolsExtension());
+}
+const composedEnhancers = compose(
+  applyMiddleware(sagaMiddleware),
+  ...enhancers
+);
+
+export default createStore(reducers, composedEnhancers);
 
 sagaMiddleware.run(rootSaga);
