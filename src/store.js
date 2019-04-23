bd0b356e904dc2a02f616d85a1a83e0008290b42
Oleksandr Bazil <oleksandrbazil@gmail.com>
2019-04-23 16:49:30 +0300

    develop: added redux-saga

--- src/store.js
+++ src/store.js
@@ -1,4 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
+import rootSaga from './sagas';
 
-export default createStore(reducers, applyMiddleware());
+const sagaMiddleware = createSagaMiddleware();
+
+export default createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
