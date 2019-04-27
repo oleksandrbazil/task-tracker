635b5c6229a069ee8d4f860c0c13fbed73eb3ba5
Oleksandr Bazil <oleksandrbazil@gmail.com>
2019-04-27 17:01:11 +0300

    Updates

---
+++ precache-manifest.6ec975965c05b19454b66b01d349bca3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3aede7f4c92b9f10910f084fddb928",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "14d42bbe0c8736afbebc",
+    "url": "/task-tracker/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "c410d1e605b01b0139a8",
+    "url": "/task-tracker/static/js/2.12f6e13a.chunk.js"
+  },
+  {
+    "revision": "14d42bbe0c8736afbebc",
+    "url": "/task-tracker/static/js/main.ac7e7a8f.chunk.js"
+  },
+  {
+    "revision": "b1db038a1d9447f463dd",
+    "url": "/task-tracker/static/js/runtime~main.89f8e460.js"
+  }
+]);
