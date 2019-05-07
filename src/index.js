48ff7b910addf713cb300e26c35558c59a97a154
Oleksandr Bazil <oleksandrbazil@gmail.com>
2019-05-07 17:11:38 +0300

    feature/improvements: moved from BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
