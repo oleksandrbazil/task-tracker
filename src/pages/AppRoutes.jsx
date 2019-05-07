1c135ead7d61b28de6a896bdf47ad348684e149c
Oleksandr Bazil <oleksandrbazil@gmail.com>
2019-05-08 01:02:49 +0300

    feature/improvements: refactored TaskTabs to make possible use Switch of react router

--- src/pages/AppRoutes.jsx
+++ src/pages/AppRoutes.jsx
@@ -2,11 +2,16 @@ import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 import HomePage from './HomePage';
 import TaskPage from './TaskPage';
+import TaskForm from '../components/TaskForm';
+import tabs from '../components/TaskTabs/tabs';
+
+const tabPaths = tabs.map(item => item.path);
 
 export default () => (
   <div>
+    <TaskForm />
     <Switch>
-      <Route path="/" exact component={HomePage} />
+      <Route path={tabPaths} exact component={HomePage} />
       <Route path="/tasks" component={TaskPage} />
       <Route render={() => <div>Page Not Found</div>} />
     </Switch>
